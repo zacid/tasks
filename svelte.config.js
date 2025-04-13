c6b7a2367eeb94a5b32cf1001ffa664de83848b9
Zac <zac156@gmail.com>
2025-04-13 20:38:02 +0200

    Github workflows

--- svelte.config.js
+++ svelte.config.js
@@ -8,14 +8,11 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '404.html'
 		}),
 		paths: {
-			base: '/tasks'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
