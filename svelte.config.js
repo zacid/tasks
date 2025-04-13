7a6a512595557b5408603592f48c709c87873a9f
Zac <zac156@gmail.com>
2025-04-13 20:29:33 +0200

    Github workflows

--- svelte.config.js
+++ svelte.config.js
@@ -12,13 +12,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			strict: false
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/tasks' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/tasks/',
 	plugins: [tailwindcss(), sveltekit()]
 });
